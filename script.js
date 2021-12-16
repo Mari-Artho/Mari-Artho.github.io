a74a0a5ba9cf9cfed29e699e38a2880f93e97ee5
Mari-Artho <mari.artho@medieinstitutet.se>
2021-12-16 17:22:15 +0100

    Add JS.

---
+++ script.js
@@ -0,0 +1,19 @@
+fetch("https://api.github.com/users/mari-artho/repos")
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (json) {
+    console.log("github API", json);
+
+    var githubRepos = document.getElementById("githubRepos");
+    for (i = 0; i < json.length; i++) {
+      githubRepos.insertAdjacentHTML(
+        "beforeend",
+        "<a href='" +
+          json[i].html_url +
+          "' target='_blank'><div class='githubrepo'>" +
+          json[i].name +
+          "</div></a>"
+      );
+    }
+  });
